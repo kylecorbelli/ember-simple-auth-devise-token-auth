5c3272d8230d0542145d089f20e0fdd6fb667c74
ngouy <nathangouy@free.fr>
2020-07-21 13:56:50 -0400

    Make it compatible with ESA v3
    `authorize` method is no more used in ESAv3
    
    Instead, it uses the `header` computed property
    
    Here is a version where everybody is happy and it works in all cases

--- addon/mixins/data-adapter-mixin.js
+++ addon/mixins/data-adapter-mixin.js
@@ -1,22 +1,31 @@
 import Mixin from '@ember/object/mixin';
 import { inject as service } from '@ember/service';
+import { computed } from "@ember/object"
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default Mixin.create(DataAdapterMixin, {
   session: service(),
 
-  authorize (xhr) {
-    const {
-      accessToken,
-      client,
-      expiry,
-      tokenType,
-      uid,
-    } = this.get('session.data.authenticated');
-    xhr.setRequestHeader('access-token', accessToken);
-    xhr.setRequestHeader('client', client);
-    xhr.setRequestHeader('expiry', expiry);
-    xhr.setRequestHeader('token-type', tokenType);
-    xhr.setRequestHeader('uid', uid);
+  // Ember-simple-auth version < 3
+  authorize(xhr) {
+    const headers = this.get("headers");
+    Object.entries(headers).map(
+      header => xhr.setRequestHeader(...header)
+    )
   },
+
+  // Ember-simple-auth version >= 3
+  headers: computed("session.data.authenticated.token", function () {
+    const { accessToken, client, expiry, tokenType, uid } = this.get(
+      "session.data.authenticated",
+    );
+
+    return {
+      "access-token": accessToken,
+      "client": client,
+      "expiry": expiry,
+      "token-type": tokenType,
+      "uid": uid,
+    };
+  }),
 });
